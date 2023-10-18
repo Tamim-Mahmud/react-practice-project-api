5e47cbfdb17dd44448476c0a307dd2973766a7bb
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-10-18 14:53:56 +0600

    date wise sorting aded

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <Button>Sort By Date</Button>
+      
       <Cart></Cart>
       
     </>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,47 +5,57 @@ import Modal from "../Modal/Modal";
 
 const Cart = () => {
   const [data, setData] = useState([]);
-  const [showAll,setShowAll]=useState(false);
-  const [uniqueId,setUniqueId]=useState(null);
+  const [showAll, setShowAll] = useState(false);
+  const [uniqueId, setUniqueId] = useState(null);
 
-  const [singleData, setSingleData]=useState({});
-
-  console.log(uniqueId);
+  const [singleData, setSingleData] = useState({});
 
   const handleShowAll = () => setShowAll(true);
 
+  const handleSort = () => {
+    let storedData = data.slice().sort((a, b) => new Date(b.published_in) - new Date(a.published_in));
+    setData(storedData);
+  };
+  console.log(data);
   useEffect(() => {
     const loadData = async () => {
-      const res = await fetch(
-        `aiData.json`
-      );
+      const res = await fetch(`aiData.json`);
       const value = await res.json();
       setData(value.data.tools);
     };
     loadData();
   }, []);
 
-  useEffect(()=>{
-    fetch(`https://openapi.programming-hero.com/api/ai/tool/${uniqueId}`).then(res =>res.json()).then(data => setSingleData(data.data))
-  }, [uniqueId])
-//  console.log(data);
+  useEffect(() => {
+    fetch(`https://openapi.programming-hero.com/api/ai/tool/${uniqueId}`)
+      .then((res) => res.json())
+      .then((data) => setSingleData(data.data));
+  }, [uniqueId]);
+  //  console.log(data);
   return (
-  <>
-    <div className="flex flex-wrap items-center justify-center gap-6 my-6">
-    {
-        data.slice(0,showAll? 12:6).map((singleData,index) => {
-            return <SingleCart key={index} setUniqueId={setUniqueId} singleData={singleData}  />
-        })
-    }
-    </div>
-    {
-      (!showAll) && <p className="mb-5" onClick={handleShowAll}>
-      <Button>Show All</Button>
-    </p>
-    }
-    <Modal singleData={singleData} />
-    
-  </>
+    <>
+      {/* { console.log((data))} */}
+      <span onClick={handleSort}>
+        <Button>Sort By Date</Button>
+      </span>
+      <div className="flex flex-wrap items-center justify-center gap-6 my-6">
+        {data?.slice(0, showAll ? 12 : 6).map((singleData, index) => {
+          return (
+            <SingleCart
+              key={index}
+              setUniqueId={setUniqueId}
+              singleData={singleData}
+            />
+          );
+        })}
+      </div>
+      {!showAll && (
+        <p className="mb-5" onClick={handleShowAll}>
+          <Button>Show All</Button>
+        </p>
+      )}
+      <Modal singleData={singleData} />
+    </>
   );
 };
 
